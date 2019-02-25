371134079685d2b83cbcd40264ed5a557e0bb0fd
Yuta Segawa <yuta.segawa@quad.co.jp>
2019-02-25 11:22:25 +0900

    feat: read urls from a file

--- screen_shot.py
+++ screen_shot.py
@@ -9,22 +9,28 @@ import chromedriver_binary
 import requests
 from urllib.parse import urlparse
 
-# TODO 外部ファイルにする
-URL = [
-    "https://quad.co.jp/",
-    "https://www.yahoo.co.jp/",
-]
-OUT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'out/')
+IN_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input/urls.txt')
+OUT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output/')
+
+USER_AGENT_IPHONE = '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) ' \
+                    'AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1'
+
+# Screen size of iPhone X
+SCREEN_W, SCREEN_H = 375, 812
 
 
 def main():
     # TODO マルチプロセスにする？
-    for i, url in enumerate(URL):
-        status = check_status(url)
+
+    with open(IN_FILE, "r") as f:
+        urls = [line.strip() for line in f.readlines()]
+
+    for i, url in enumerate(urls):
+        status_code = check_status(url)
 
         parsed = urlparse(url)
         path = parsed.path.replace('/', '_')
-        out = OUT_PATH + "{}-{}-{}.png".format(i, status, path)
+        out = OUT_PATH + "{}-{}-{}.png".format(i, status_code, path)
 
         capture(url, out)
 
@@ -36,18 +42,10 @@ def check_status(url):
 
 def capture(url, out):
     options = webdriver.ChromeOptions()
-    options.add_argument(
-        '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92 Safari/602.1')
+    options.add_argument(USER_AGENT_IPHONE)
     driver = webdriver.Chrome(options=options)
-
-    # w = driver.execute_script('return document.body.scrollWidth')
-    # h = driver.execute_script('return document.body.scrollHeight')
-
-    # Screen size of iPhone X
-    w, h = 375, 812
-
     driver.set_page_load_timeout(15)
-    driver.set_window_size(w, h)
+    driver.set_window_size(SCREEN_W, SCREEN_H)
 
     try:
         driver.get(url)
